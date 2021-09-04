a4a0eff5ab74a6d8ddacaf9b074bd7ba54a58cb1
wxcdz <812150635@qq.com>
2021-09-04 18:05:17 +0800

    python整数类型

---
+++ 01-变量类型和运算符/02-python整数类型int.py
@@ -0,0 +1,39 @@
+
+a = 78
+print(a)
+print(type(a))
+# <class 'int'>
+
+# 十六进制
+hex1 = 0x45
+hex2 = 0x4AF
+
+print("hex1Value:", hex1)
+# hex1Value: 69
+print("h3x2Value:", hex2)
+# h3x2Value: 1199
+
+
+# 二进制
+bin1 = 0b10
+print("bin1Value:", bin1)
+# Output: bin1Value: 2
+bin2 = 0b110
+print("bin2Value:", bin2)
+# Output: bin2Value: 6
+
+#八进制
+oct1 = 0o26
+print("oct1Value:", oct1)
+# Output: oct1Value: 22
+oct2 = 0O41
+print("oct2Value:", oct2)
+# Output: oct2Value: 33
+
+# 数字分隔符
+click = 1_301_547
+distance = 384_000_000
+print("Python教程阅读量：", click)
+# Python教程阅读量： 1301547
+print("地球和月球的距离：", distance)
+# 地球和月球的距离： 384000000
