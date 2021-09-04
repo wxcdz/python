2e935d19a04b54dc9686abcdd3430f9fd468a85a
wxcdz <812150635@qq.com>
2021-09-04 18:17:24 +0800

    python编码格式

---
+++ 01-变量类型和运算符/05-Python字符串的编码格式.py
@@ -0,0 +1,12 @@
+import sys
+
+print(sys.getdefaultencoding())
+# Output: utf-8
+
+
+# 在 Python 3.x 中也可以用 ord() 和 chr() 函数实现字符和编码数字之间的转换
+print(chr(81))
+# Output: Q
+
+print(ord('王'))
+# Output: 29579
