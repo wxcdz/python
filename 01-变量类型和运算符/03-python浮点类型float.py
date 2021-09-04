b073a65742afeec3133fcaa745475c35f7d747c6
wxcdz <812150635@qq.com>
2021-09-04 18:06:04 +0800

    python浮点类型

---
+++ 01-变量类型和运算符/03-python浮点类型float.py
@@ -0,0 +1,19 @@
+f1 = 12.5
+print("f1Value: ", f1)
+print("f1Type: ", type(f1))
+# Output: f1Type:  <class 'float'>
+
+f3 = 0.0000000000000000000000000847
+print("f3Value: ", f3)
+print("f3Type: ", type(f3))
+# f3Value:  8.47e-26
+# f3Type:  <class 'float'>
+f4 = 345679745132456787324523453.45006
+print("f4Value: ", f4)
+print("f4Type: ", type(f4))
+
+f6 = 12.3 * 0.1
+print("f6Value: ", f6)
+print("f6Type: ", type(f6))
+# f4Value:  3.456797451324568e+26
+# f4Type:  <class 'float'>
